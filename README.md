d4a7d9e49de216fa616be55baeaf15e500c4ab62
Tanya <tatyana-baeva@list.ru>
2023-11-20 22:55:02 +0700

    Create app for image recognition

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # third_task
+
+# curl -X 'POST' 'http://127.0.0.1:8000/prediction/' -H 'Content-Type: application/json' -d '{"url": "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg"}'
+# {"model-prediction":"Egyptian_cat","model-prediction-confidence-score":"0.5804819"}
--- main.py
+++ main.py
@@ -1,21 +1,55 @@
 from fastapi import FastAPI
-from transformers import pipeline
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
+
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input
+from tensorflow.keras.applications.efficientnet import decode_predictions
+from tensorflow.keras.utils import get_file 
+import numpy as np
+
 
 
 class Item(BaseModel):
-    text: str
+    url: HttpUrl
 
 app = FastAPI()
-classifier = pipeline('sentiment-analysis')
 
 @app.get('/')
 async def root():
-    return {'message': 'Hello world!'}
-
-@app.post('/predict/')
-def predict(item: Item):
-    return classifier(item.text)[0]
+    return {'message': 'Welcome!'}
 
 
+def load_model():
+    model = EfficientNetB0(weights='imagenet')
+    return model
 
+
+def preprocess_image(img):
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+
+@app.post("/prediction/")
+async def get_net_image_prediction(item: Item):
+    if item.url == "":
+        print(item.url)
+        return {"message": "No image link provided"}
+    
+    img = image.load_img(get_file('image', str(item.url)),target_size=(224, 224))
+  
+
+    x = preprocess_image(img)
+    
+
+    model = load_model()
+    pred = model.predict(x)
+    classes = decode_predictions(pred, top=1)[0]
+    for i in classes:
+        model_prediction = str(i[1])
+        model_prediction_confidence_score = str(i[2])
+
+        return {"model-prediction": model_prediction, "model-prediction-confidence-score": model_prediction_confidence_score}
+    

de315a5b884dd260cfa114d8973d69dc5f136662
Tanya <tatyana-baeva@list.ru>
2023-11-28 17:59:47 +0700

    test predict

--- test_main.py
+++ test_main.py
@@ -10,8 +10,8 @@ def test_read_main():
     assert response.json() == {'message': 'Welcome!'}
 
 def test_predict():
-    response = client.get('/prediction/',
-                          json={"url": "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg"})
+    response = client.post('/prediction/',
+                           json={"url": "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['model-prediction'] == 'Egyptian_cat'
